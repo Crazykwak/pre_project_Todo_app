caa7a9c9edf5bc9092706f75ca5345afa2d91d1f
Crazykwak <kwj1830@naver.com>
2022-08-22 13:33:19 +0900

    first commit

---
+++ src/main/java/Team_ParkingLot/pre_project_Todo_app/PreProjectTodoAppApplication.java
@@ -0,0 +1,13 @@
+package Team_ParkingLot.pre_project_Todo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team_ParkingLot/pre_project_Todo_app/PreProjectTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Team_ParkingLot.pre_project_Todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
